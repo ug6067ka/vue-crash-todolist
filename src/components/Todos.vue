3c384eac4cc817eea1479460bfad70542eeb520e
ArisaKamishima <arisa.6.snsd@icloud.com>
2020-01-08 22:40:54 +1100

    Create new one for practice

---
+++ src/components/Todos.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div v-for="todo in todos" v-bind:key="todo.id">
+      <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo',todo.idd)" />
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from "./TodoItem.vue";
+export default {
+  name: "Todos",
+  props: ["todos"],
+  components: {
+    TodoItem
+  }
+};
+</script>
+
+
+<style scoped>
+</style>
